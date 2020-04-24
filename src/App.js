0d584dd45cb03a1425c538084b99c68719bfb96b
seanmx96 <seanmx96@users.noreply.github.com>
2020-04-24 11:39:09 -0500

    Completed pizza order form. Setup change handlers. Initialized form values, error values, and formSchema.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { Route, Switch, Link } from 'react-router-dom'
 import axios from 'axios'
 import * as yup from 'yup'
 
+import OrderForm from './OrderForm'
+import imageSrc from './Assets/Pizza.jpg'
+
 //////////// URL FOR [POST] REQUESTS /////////////////////
 const url = 'https://reqres.in/api/users'
 
 const initialFormValues = {
-  ///// TEXT INPUTS /////
+  ///// TEXT INPUT /////
   name: '',
   ///// DROPDOWN /////
   pizzaSize: '',
@@ -19,14 +22,135 @@ const initialFormValues = {
     chicken: false,
     olives: false
   },
+  ///// TEXT INPUT /////
+  specialInstructions: '',
 }
 
+///////// THE SHAPE OF THE VALIDATION ERRORS OBJECT ///////////
+const initialFormErrors = {
+  name: '',
+  pizzaSize: '',
+}
+
+///////// FORM SCHEMA FOR VALIDATION ///////////
+const formSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(2, 'name must have at least 2 characters!')
+    .required('name is required!'),
+  pizzaSize: yup
+    .string(),
+  components: yup
+    .boolean(),
+  specialInstructions: yup
+    .string()
+})
+
 const App = () => {
+  ////////////////// STATE TO KEEP TRACK OF FORM VALUES //////////////////
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  ////////////////// STATE TO KEEP TRACK OF VALIDATION ERRORS //////////////////
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+
+  ////////////////// STATE TO KEEP TRACK OF ORDERS //////////////////
+  const [orders, setOrders] = useState([])
+
+  ////////////////// IMPLEMENT CHANGE HANDLERS /////////////////////////
+  const postOrder = (order) => {
+    axios.post(url, order)
+      .then(res => {
+        console.log(res)
+        console.log(res.data)
+        setOrders([...orders, res.data])
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  const onInputChange = evt => {
+
+    const name = evt.target.name
+    const value = evt.target.value
+
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+
+      .then(valid => {
+        // CLEAR ERROR
+        setFormErrors({
+          ...formErrors,
+          [name]: '',
+        })
+      })
+      .catch(err => {
+        // SET THE ERROR IN THE RIGHT PLACE
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        })
+      })
+    
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
+
+  const onCheckboxChange = evt => {
+    const name = evt.target.name
+    const isChecked = evt.target.checked
+
+    setFormValues({
+      ...formValues,
+      components: {
+        ...formValues.components,
+        [name]: isChecked,
+      }
+    })
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+
+    const newOrder = {
+      name: formValues.name,
+      pizzaSize: formValues.pizzaSize,
+      components: {
+        pepperoni: formValues.components.pepperoni,
+        mushroom: formValues.components.mushroom,
+        chicken: formValues.components.chicken,
+        olives: formValues.components.olives,
+      },
+      specialInstructions: formValues.specialInstructions,
+    }
+
+    postOrder(newOrder)
+    setFormValues(initialFormValues)
+  }
+
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
-      <button>Order Pizza!!</button>
-    </>
+      <img src={imageSrc} />
+      <Switch>
+        <Route path='/pizza'>
+          <OrderForm 
+            values={formValues}
+            onInputChange={onInputChange}
+            onCheckboxChange={onCheckboxChange}
+            onSubmit={onSubmit}
+            errors={formErrors}
+          />
+        </Route>
+
+        <Route path='/'>
+          <Link to='/pizza' className='orderButton'><button>Order Pizza!!</button></Link>
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
--- src/OrderForm.jsx
+++ src/OrderForm.jsx
@@ -13,6 +13,70 @@ function OrderForm(props) {
     return (
         <form className='container'>
             <h2>Pizza Order Form</h2>
+            <div className='errors'>
+                <h3>{errors.name}</h3>
+                <h3>{errors.toppings}</h3>
+                <h3>{errors.submit}</h3>
+            </div>
+
+            {/* ///////////////////// TEXT INPUT ////////////////// */}
+            <label>Name:&nbsp;&nbsp;&nbsp;
+                <input
+                    value={values.name}
+                    onChange={onInputChange}
+                    name='name'
+                    type='text'
+                />
+            </label><br /><br />
+
+            {/* ////////// DROPDOWN ////////// */}
+            <label>Pizza Size:&nbsp;
+            <select
+                value={values.pizzaSize}
+                onChange={onInputChange}
+                name='pizzaSize'
+            >
+                <option defaultValue=''>Please choose</option>
+                <option value='small'>small</option>
+                <option value='medium'>medium</option>
+                <option value='large'>large</option>
+            </select></label><br /><br />
+
+            {/* ////////// CHECKBOXES ////////// */}
+            <label><input
+                checked={values.components.pepperoni}
+                onChange={onCheckboxChange}
+                name='pepperoni'
+                type="checkbox" /> Pepperoni</label>&nbsp;
+            <label><input
+                checked={values.components.mushroom}
+                onChange={onCheckboxChange}
+                name='mushroom'
+                type="checkbox" /> Mushroom</label>&nbsp;<br /><br />
+            <label><input
+                checked={values.components.chicken}
+                onChange={onCheckboxChange}
+                name='chicken'
+                type="checkbox" /> Chicken</label>&nbsp;
+            <label><input
+                checked={values.components.olives}
+                onChange={onCheckboxChange}
+                name='olives'
+                type="checkbox" /> Olives</label><br /><br />
+
+            {/* ///////////////////// SPECIAL INSTRUCTIONS TEXT INPUT ////////////////// */}
+            <label>Special Instructions:&nbsp;&nbsp;&nbsp;
+                <input
+                    value={values.specialInstructions}
+                    onChange={onInputChange}
+                    name='specialInstructions'
+                    type='text'
+                />
+            </label><br /><br />
+
+            <button onClick={onSubmit}>Add to Order</button>
         </form>
     )
 }
+
+export default OrderForm
